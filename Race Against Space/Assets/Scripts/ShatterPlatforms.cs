093a71167b0fbf13f5497d6cd13e2bc5f58a7f63
ShinobiSpartan <36467680+ShinobiSpartan@users.noreply.github.com>
2018-10-18 16:55:47 +1100

    - Added "Shatter Platform" Script

---
+++ Race Against Space/Assets/Scripts/ShatterPlatforms.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterPlatforms : MonoBehaviour {
+
+    public int timeBeforeShatter = 3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject, 1 + timeBeforeShatter);
+        }
+    }
+}
