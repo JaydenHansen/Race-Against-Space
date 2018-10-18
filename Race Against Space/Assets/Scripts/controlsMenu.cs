9fc366a08498afa63b623709519f6c5b4342adca
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-10-18 15:05:04 +1100

    committing the function that will pause the game and show the controls, work in progress (very bad)

---
+++ Race Against Space/Assets/Scripts/controlsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class controlsMenu : MonoBehaviour {
+
+	// Use this for initialization
+    public XboxController controller;
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (XCI.GetButtonDown(XboxButton.Start, controller))
+        {
+            Time.timeScale = 1;
+        }
+	}
+}
