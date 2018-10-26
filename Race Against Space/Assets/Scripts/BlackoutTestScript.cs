5be4aa73db3ef32214ba46e3635bbe4fe9d48f9c
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-10-26 14:28:38 +1100

    Worked on the energy bar going down in the UI. Made a simple test function for the blackout event.

---
+++ Race Against Space/Assets/Scripts/BlackoutTestScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackoutTestScript : MonoBehaviour {
+    private bool isBlackout = false;
+    private float timer = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > 5.0f && !isBlackout)
+        {
+            isBlackout = true;
+            timer = 0.0f;
+        }
+        if (timer > 5.0f && isBlackout)
+        {
+            isBlackout = false;
+            timer = 0.0f;
+        }
+        if (isBlackout)
+        {
+            this.GetComponent<Light>().enabled = false;
+        }
+        if (!isBlackout)
+        {
+            this.GetComponent<Light>().enabled = true;
+        }
+    }
+}
---
+++ Race Against Space/Assets/Scripts/energyReduce.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class energyReduce : MonoBehaviour {
+
+    public PlayerController playerMove;
+    float e; 
+    // Use this for initialization
+    private Vector3 originalPosition;
+    private void Awake()
+    {
+        originalPosition = transform.position;
+    }
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        e = playerMove.energy;
+        //transform.Translate(new Vector3(-(e/100), 0, 0) * Time.deltaTime);
+        transform.Translate(new Vector3(-(0.15f), 0, 0) * Time.deltaTime);
+
+
+        //transform.Translate(new Vector3((originalPosition.x - e), 0, 0));
+        //if(e <= 100 && e > 80)
+        //{
+        //    transform.Translate(originalPosition);
+        //}
+        //if (e <= 80 && e > 60)
+        //{
+        //    transform.Translate(new Vector3(((originalPosition.x - 1) / 10), 0, 0) * Time.deltaTime);
+        //}
+        //if (e <= 60 && e > 40)
+        //{
+        //    transform.Translate(new Vector3(((originalPosition.x - 2) / 10), 0, 0) * Time.deltaTime);
+        //}
+        //if (e <= 40 && e > 20)
+        //{
+        //    transform.Translate(new Vector3(((originalPosition.x - 3) / 10), 0, 0) * Time.deltaTime);
+        //}
+        //if (e <= 20 && e >= 0)
+        //{
+        //    transform.Translate(new Vector3(((originalPosition.x - 2) / 10), 0, 0) * Time.deltaTime);
+        //}
+    }
+}
