04720948d43b051ab0c5c1e06da8bdd7a359b39a
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-10-26 14:47:15 +1100

    - Added comments.

--- Race Against Space/Assets/Scripts/BlackoutTestScript.cs
+++ Race Against Space/Assets/Scripts/BlackoutTestScript.cs
@@ -13,24 +13,30 @@ public class BlackoutTestScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //gets the timer ready
         timer += Time.deltaTime;
+        //activates blackout after 5 seconds
         if(timer > 5.0f && !isBlackout)
         {
-            isBlackout = true;
-            timer = 0.0f;
+            isBlackout = true;//blackout activates
+            timer = 0.0f;//timer is set back to 0    
         }
+
+        //turns blackout off after 5 seconds of blackout
         if (timer > 5.0f && isBlackout)
         {
-            isBlackout = false;
-            timer = 0.0f;
+            isBlackout = false;//blackout deactivates 
+            timer = 0.0f;//timer set back to 0
         }
         if (isBlackout)
         {
+            //if the blackout is active it will turn off the level light and increase the intensity of the light the player gives off
             this.GetComponent<Light>().enabled = false;
             playerGlow.intensity = 20; 
         }
         if (!isBlackout)
         {
+            //if blackout is inactive the level light will go on and the player's light intensity will be reduced
             this.GetComponent<Light>().enabled = true;
             playerGlow.intensity = 5;
         }
