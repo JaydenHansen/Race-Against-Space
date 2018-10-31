ecea9b26e63bb7de58f1adfb84db5df0171a39ca
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-11-01 10:33:27 +1100

    - Added text that says you dead n stuff.

--- Race Against Space/Assets/Scripts/energyReduce.cs
+++ Race Against Space/Assets/Scripts/energyReduce.cs
@@ -6,14 +6,24 @@ using UnityEngine.UI;
 public class energyReduce : MonoBehaviour {
 
     public PlayerController playerMove;
-    public Slider energyBar; 
+    public Slider energyBar;
+    public Text deathText;
     // Use this for initialization
     void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.Translate(new Vector3(-(0.15f), 0, 0) * Time.deltaTime);
         energyBar.value = playerMove.energy;
+        if(energyBar.value <= 0)
+        {
+            deathText.IsActive();
+            deathText.text = "Fookin dead";
+            Destroy(energyBar);
+        }
+        if(playerMove == null)
+        {
+            energyBar.value = 0; 
+        }
     }
 }
