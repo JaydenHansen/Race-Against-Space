08b5153eb2ef47ec35d8b7308966e56c1069bf49
ShinobiSpartan <36467680+ShinobiSpartan@users.noreply.github.com>
2018-11-01 16:00:52 +1100

    - Removed "PhaseThroughPlatforms" Script - Added "PlatformPhasing" Script (Improved version of previous point) - Edited Pickup Prefab (Added trigger) - Edited Nathan's Scene

---
+++ Race Against Space/Assets/Scripts/PlatformPhasing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPhasing : MonoBehaviour
+{
+
+    public Collider playerCollider;
+    public Collider platformAbove;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCollider = this.gameObject.GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        platformAbove = other.gameObject.GetComponent<Collider>();
+
+        Physics.IgnoreCollision(playerCollider, platformAbove, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Physics.IgnoreCollision(playerCollider, platformAbove, false);
+
+        platformAbove = null;
+    }
+}
