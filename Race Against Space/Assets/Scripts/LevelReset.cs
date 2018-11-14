913e7bdedc1e9ba355873bb4087144d89d0c6de5
ShinobiSpartan <36467680+ShinobiSpartan@users.noreply.github.com>
2018-11-14 16:46:34 +1100

    - Fixed "LevelReset" Script (+ Added slow time)

--- Race Against Space/Assets/Scripts/LevelReset.cs
+++ Race Against Space/Assets/Scripts/LevelReset.cs
@@ -4,97 +4,118 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelReset : MonoBehaviour
-{ 
+{
     public GameObject[] listOfPlayers;
+   // public GameObject roundWinPlayer = null;
 
-    public float roundEndWaitTimer = 3f;
-    private float timer;
+    public int endSceneIndex = 1;
+
+    public float timer = 1.0f;
+    private float countdown;
+
+    public float slowTime = 0.05f;
+    public bool hasSlowed = false;
 
     // Use this for initialization
     void Awake()
     {
         listOfPlayers = GameObject.FindGameObjectsWithTag("Player");
-        timer = roundEndWaitTimer;
+        countdown = timer;
     }
 
     // Update is called once per frame  
     void Update()
-    { 
-        Debug.Log(ScoreManager.player1Score);
-
+    {
         listOfPlayers = GameObject.FindGameObjectsWithTag("Player");
 
-        if(listOfPlayers.Length <= 1)
+        if (listOfPlayers.Length <= 1)
         {
-            if(listOfPlayers[0].name.Equals("Mesh_Character_Full_01"))
+            AddRoundTally();
+        }
+    }
+
+    void AddRoundTally()
+    {
+        if(hasSlowed == false)
+        {
+            Time.timeScale -= 0.005f;
+        }
+        if (Time.timeScale <= slowTime)
+        {
+            hasSlowed = true;
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown <= 0)
+        { 
+            if (listOfPlayers[0].name.Equals("Mesh_Character_Full_01"))
             {
-                if(ScoreManager.player1Score < 2)
-                { 
-                    timer -= Time.deltaTime;
-                    if (timer <= 0)
-                    {
-                        ScoreManager.player1Score++;
-                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                        timer = roundEndWaitTimer;
-                    }
+                if (ScoreManager.player1Score < 2)
+                {
+                    ScoreManager.player1Score++;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    Time.timeScale = 1;
                 }
                 else
                 {
-                    SceneManager.LoadScene(1);
+                    SceneManager.LoadScene(endSceneIndex);
+                    Time.timeScale = 1;
+                    ScoreReset();
                 }
             }
-			else if (listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (1)"))
+            else if (listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (1)"))
             {
-              if (ScoreManager.player2Score < 2)
-              {
-                 timer -= Time.deltaTime;
-                 if (timer <= 0)
-                 {
-                     ScoreManager.player2Score++;
-                     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                     timer = roundEndWaitTimer;
-                 }
-              }
-              else
-              {
-                  SceneManager.LoadScene(1);
-              }
-           }
-			if(listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (2)"))
-			{
-				if(ScoreManager.player3Score < 2)
-				{ 
-					timer -= Time.deltaTime;
-					if (timer <= 0)
-					{
-						ScoreManager.player3Score++;
-						SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-						timer = roundEndWaitTimer;
-					}
-				}
-				else
-				{
-					SceneManager.LoadScene(1);
-				}
-			}
-			if(listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (3)"))
-			{
-				if(ScoreManager.player4Score < 2)
-				{ 
-					timer -= Time.deltaTime;
-					if (timer <= 0)
-					{
-						ScoreManager.player4Score++;
-						SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-						timer = roundEndWaitTimer;
-					}
-				}
-				else
-				{
-					SceneManager.LoadScene(1);
-				}
-			}
-
+                if (ScoreManager.player2Score < 2)
+                {
+                    ScoreManager.player2Score++;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    Time.timeScale = 1;
+                }
+                else
+                {
+                    SceneManager.LoadScene(endSceneIndex);
+                    Time.timeScale = 1;
+                    ScoreReset();
+                }
+            }
+            else if (listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (2)"))
+            {
+                if (ScoreManager.player3Score < 2)
+                {
+                    ScoreManager.player3Score++;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    Time.timeScale = 1;
+                }
+                else
+                {
+                    SceneManager.LoadScene(endSceneIndex);
+                    Time.timeScale = 1;
+                    ScoreReset();
+                }
+            }
+            else if (listOfPlayers[0].name.Equals("Mesh_Character_Full_01 (3)"))
+            {
+                if (ScoreManager.player4Score < 2)
+                {
+                    ScoreManager.player4Score++;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    Time.timeScale = 1;
+                }
+                else
+                {
+                    SceneManager.LoadScene(endSceneIndex);
+                    Time.timeScale = 1;
+                    ScoreReset();
+                }
+            }
         }
+    }
+
+    void ScoreReset()
+    {
+        ScoreManager.player1Score = 0;
+        ScoreManager.player2Score = 0;
+        ScoreManager.player3Score = 0;
+        ScoreManager.player4Score = 0;
     }
 }
