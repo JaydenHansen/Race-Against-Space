9da067b17ac4c785cc1b3aeda57f4d1fcc62dc16
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-11-21 15:30:13 +1100

    - Added comments to the main menu script

--- Race Against Space/Assets/Scripts/mainMenu.cs
+++ Race Against Space/Assets/Scripts/mainMenu.cs
@@ -6,18 +6,22 @@ using UnityEngine.SceneManagement;
 public class mainMenu : MonoBehaviour {
     public void PlayGame()
     {
+        //when play game is selected it loads the next scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
     {
+        //if quit game is selected it quits game
         Application.Quit(); 
     }
     public void ReturnToMenu()
     {
+        //if return to menu is selected load the first scene
         SceneManager.LoadScene(0);
     }
     public void ReturnToCharacterSelect()
     {
+        //if return to character select is selected return to character select
         SceneManager.LoadScene(1);
     }
 }
