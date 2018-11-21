4e0eefe97633eea5475d85092fe408ccfe1eec88
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-11-21 16:08:20 +1100

    - Commented on ScoreManager script.

--- Race Against Space/Assets/Scripts/ScoreManager.cs
+++ Race Against Space/Assets/Scripts/ScoreManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour {
-
+    //sets the initial scores of the players to 0 so the start of the game everyone has 0 points
     public static int player1Score = 0;
     public static int player2Score = 0;
     public static int player3Score = 0;
