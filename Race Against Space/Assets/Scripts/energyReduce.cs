7156b26694b42555cf1db5310bf484f2a12a8c66
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-11-21 15:24:54 +1100

    - Added comments to the energy reduce script

--- Race Against Space/Assets/Scripts/energyReduce.cs
+++ Race Against Space/Assets/Scripts/energyReduce.cs
@@ -12,33 +12,35 @@ public class energyReduce : MonoBehaviour {
     public RawImage playerIcon;
     // Use this for initialization
     void Start () {
-        PlayerUI.SetActive(true);
+        PlayerUI.SetActive(true);//sets the player UI to active
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //PlayerUI.SetActive(true);
+        //if player is null sets their energy to 0
         if (playerMove == null)
         {
             energyBar.value = 0;
         }
         else
         {
+            //if the player is active set the energy value to be the players energy
             energyBar.value = playerMove.playerEnergy;
         }
+        //if the energy is less than 0 
         if(energyBar.value <= 0)
         {
+            //display connection lost and destroy their icon and remove their energy bar
             deathText.text = "Connection Lost";
             Destroy(energyBar);
             Destroy(playerIcon);
         }
         
+        //if the player is inactive in the hieracy set the energy to 0
         if (!playerMove.gameObject.activeInHierarchy)
         {
-            //PlayerUI.SetActive(false); 
             energyBar.value = 0;
-            //PlayerUI.SetActive(false); 
-            
         }
        
     }
