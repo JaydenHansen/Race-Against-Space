815b97d1499aad7c60dabd7a4f2466d139437191
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2018-11-21 15:16:38 +1100

    - Added comments in the EnergyPickup code

--- Race Against Space/Assets/Scripts/EnergyPickup.cs
+++ Race Against Space/Assets/Scripts/EnergyPickup.cs
@@ -8,10 +8,13 @@ public class EnergyPickup : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
+        //checks to see if the object colliding with them is the player
         PlayerController player = other.gameObject.GetComponent<PlayerController>();
 
+        //checks to see if the player is active
         if (player != null)
         {
+            //increases the players energy if the interact with the energy pickup
             player.playerEnergy += pickupAmount;
             gameObject.SetActive(false);
         }
